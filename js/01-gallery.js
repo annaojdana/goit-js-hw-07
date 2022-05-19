2f4e2a395bc16a8659e67ad760ec52f2a5987852
Anna Ojdana <anna.ojdana@gmail.com>
2022-05-19 15:38:24 +0200

    run basiclightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+console.log(gallery);
+
+const markup = galleryItems
+  .map((item) => `<div class="gallery__item">
+  <a class="gallery__link" href="xx${item.preview}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`)
+  .join("");
+    
+gallery.insertAdjacentHTML("afterbegin", markup);
+gallery.addEventListener('click', (event) => {
+  
+    const imageLink = event.target.dataset.source;
+    console.log(imageLink);
+    basicLightbox.create(`
+		<img src="${imageLink}">
+	`).show()
+})   
+
+
+
