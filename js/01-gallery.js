97788a5caae6256dcdba4870e641352ecdb750e7
Anna Ojdana <anna.ojdana@gmail.com>
2022-05-21 14:48:58 +0200

    task01 - ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,31 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const gallery = document.querySelector('.gallery');
-console.log(gallery);
 
 const markup = galleryItems
   .map((item) => `<div class="gallery__item">
-  <a class="gallery__link" href="${item.preview}">
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source="${item.original}"
-      alt="${item.description}"
-    />
-  </a>
-</div>`)
+                    <a class="gallery__link" href="${item.preview}">
+                      <img
+                        class="gallery__image"
+                        src="${item.preview}"
+                        data-source="${item.original}"
+                        alt="${item.description}"
+                      />
+                    </a>
+                  </div>`
+  )
   .join("");
     
 gallery.insertAdjacentHTML("afterbegin", markup);
-gallery.addEventListener('click', (event) => {
+gallery.addEventListener('click', openModal);
+
+
+function openModal(event) { 
   if (event.target.nodeName !== "IMG") {
     return;
   }
-    const imageLink = event.target.dataset.source;
-    const instance = basicLightbox.create(`
-		<img src="${imageLink}">
-	`).show()
-})   
+  event.preventDefault(); 
+  const imageLink = event.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src="${imageLink}">`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", e => {
+          if (e.code === "Escape") {
+            instance.close();
+          }
+        });
+        
+      }
+    }  
+    );
+  instance.show();
+}  
 
 
 
